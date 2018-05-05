021145fdb284f0838b8286849b087f4ac7ad3907
Bill Wang <billhhh@users.noreply.github.com>
2018-05-05 12:22:33 +0800

    init

---
+++ shuffle_txt.py
@@ -0,0 +1,8 @@
+import random
+in_file = 'P2.csv'
+out_file = 'train_11w_shuffle.csv'
+
+data = [x.strip() for x in open(in_file)]
+random.shuffle(data)
+with open(out_file, 'w') as f:
+    f.write('\n'.join(data))
